5274c2a0e37ca4eb187d86ec36187e672557f83c
Bruno Deferrari <utizoc@gmail.com>
2009-12-24 18:44:10 -0200

    Add formset clean method test.

--- test/formsets_test.rb
+++ test/formsets_test.rb
@@ -6,8 +6,19 @@ class SimpleForm < Bureaucrat::Forms::Form
   field :name, CharField.new
 end
 
-SimpleFormFormSet = Bureaucrat::Formsets.make_formset_class(SimpleForm,
-                                                            :extra => 2)
+SimpleFormFormSet =
+  Bureaucrat::Formsets.make_formset_class(SimpleForm, :extra => 2)
+
+class CustomFormSet < Bureaucrat::Formsets::BaseFormSet
+  def clean
+    raise FieldValidationError.new('This is wrong!')
+  end
+end
+
+SimpleFormCustomFormSet =
+  Bureaucrat::Formsets.make_formset_class(SimpleForm,
+                                          :extra => 2,
+                                          :formset => CustomFormSet)
 
 class TestFormset < BureaucratTestCase
   describe 'formset with empty data' do
@@ -46,6 +57,27 @@ class TestFormset < BureaucratTestCase
       expected = [{:name => 'Lynch'}, {:name => 'Tio'}]
       result = @valid_bound_set.cleaned_data
       assert_equal(expected, result)
+    end
+  end
+
+  describe "Formset with clean method raising a FieldValidationError exception" do
+    setup do
+      management_form_data = {
+        :'form-TOTAL_FORMS' => '2',
+        :'form-INITIAL_FORMS' => '2'
+      }
+      valid_data = {:'form-0-name' => 'Lynch', :'form-1-name' => 'Tio'}
+      @bound_set =
+        SimpleFormCustomFormSet.new(management_form_data.merge(valid_data))
+    end
+
+    should 'not be valid' do
+      assert_equal(false, @bound_set.valid?)
+    end
+
+    should 'add clean errors to nonfield errors' do
+      @bound_set.valid?
+      assert_equal(["This is wrong!"],  @bound_set.non_form_errors)
     end
   end
 end
