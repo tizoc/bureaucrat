368fd4b49dcc0c47a0eee45431539c3f9cefbc3f
russell muetzelfeldt <russm@slofith.org>
2010-04-16 13:25:53 +1000

    tests for OrderedHash

---
+++ test/utils_test.rb
@@ -0,0 +1,34 @@
+require File.dirname(__FILE__) + "/test_helper"
+
+class TestUtils < BureaucratTestCase
+  describe 'OrderedHash' do
+    setup do
+      @hash = Utils::OrderedHash.new
+      @hash[:first] = "able"
+      @hash[:second] = "baker"
+      @hash[:third] = "charlie"
+      @hash[:fourth] = "delta"
+    end
+
+    should 'iterate in order' do
+      expected = [:first, "able", :second, "baker", :third, "charlie", :fourth, "delta"]
+      actual = []
+      @hash.each do |k,v|
+        actual << k
+        actual << v
+      end
+      assert_equal(expected, actual)
+    end
+
+    should 'have deletable items' do
+      expected = [:first, "able", :third, "charlie", :fourth, "delta"]
+      actual = []
+      @hash.delete :second
+      @hash.each do |k,v|
+        actual << k
+        actual << v
+      end
+      assert_equal(expected, actual)
+    end
+  end
+end
