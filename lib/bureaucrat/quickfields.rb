9c51468e9601d9c5a4b41745c9fb337cb679915a
Bruno Deferrari <utizoc@gmail.com>
2011-05-23 20:41:17 -0300

    Spacing, remove reference field type that doesn't exist anymore.

--- lib/bureaucrat/quickfields.rb
+++ lib/bureaucrat/quickfields.rb
@@ -15,81 +15,77 @@ module Bureaucrat
     end
 
     # Declare a +CharField+ with text input widget
-    def string(name, options={})
+    def string(name, options = {})
       field name, CharField.new(options)
     end
 
     # Declare a +CharField+ with text area widget
-    def text(name, options={})
+    def text(name, options = {})
       field name, CharField.new(options.merge(:widget => Widgets::Textarea.new))
     end
 
     # Declare a +CharField+ with password widget
-    def password(name, options={})
+    def password(name, options = {})
       field name, CharField.new(options.merge(:widget => Widgets::PasswordInput.new))
     end
 
     # Declare an +IntegerField+
-    def integer(name, options={})
+    def integer(name, options = {})
       field name, IntegerField.new(options)
     end
 
     # Declare a +BigDecimalField+
-    def decimal(name, options={})
+    def decimal(name, options = {})
       field name, BigDecimalField.new(options)
     end
 
     # Declare a +RegexField+
-    def regex(name, regexp, options={})
+    def regex(name, regexp, options = {})
       field name, RegexField.new(regexp, options)
     end
 
     # Declare an +EmailField+
-    def email(name, options={})
-      if !options.delete(:check_mx) then
-        field name, EmailField.new(options)
-      else
-        field name, DeliverableEmailField.new(options)
-      end
+    def email(name, options = {})
+      field name, EmailField.new(options)
     end
 
     # Declare a +FileField+
-    def file(name, options={})
+    def file(name, options = {})
       field name, FileField.new(options)
     end
 
     # Declare a +BooleanField+
-    def boolean(name, options={})
+    def boolean(name, options = {})
       field name, BooleanField.new(options)
     end
 
     # Declare a +NullBooleanField+
-    def null_boolean(name, options={})
+    def null_boolean(name, options = {})
       field name, NullBooleanField.new(options)
     end
 
     # Declare a +ChoiceField+ with +choices+
-    def choice(name, choices=[], options={})
+    def choice(name, choices = [], options = {})
       field name, ChoiceField.new(choices, options)
     end
 
     # Declare a +TypedChoiceField+ with +choices+
-    def typed_choice(name, choices=[], options={})
+    def typed_choice(name, choices = [], options = {})
       field name, TypedChoiceField.new(choices, options)
     end
 
     # Declare a +MultipleChoiceField+ with +choices+
-    def multiple_choice(name, choices=[], options={})
+    def multiple_choice(name, choices = [], options = {})
       field name, MultipleChoiceField.new(choices, options)
     end
 
     # Declare a +ChoiceField+ using the +RadioSelect+ widget
-    def radio_choice(name, choices=[], options={})
+    def radio_choice(name, choices = [], options = {})
       field name, ChoiceField.new(choices, options.merge(:widget => Widgets::RadioSelect.new))
     end
 
     # Declare a +MultipleChoiceField+ with the +CheckboxSelectMultiple+ widget
-    def checkbox_multiple_choice(name, choices=[], options={})
+    def checkbox_multiple_choice(name, choices = [], options = {})
       field name, MultipleChoiceField.new(choices, options.merge(:widget => Widgets::CheckboxSelectMultiple.new))
     end
   end
