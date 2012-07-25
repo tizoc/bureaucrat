2e1cc2f6d299b83c2a700230299fbf7d0ae52796
Myles Megyesi <mylesmegyesi@me.com>
2012-07-25 08:51:16 -0500

    adds tests

---
+++ spec/fields/boolean_field_spec.rb
@@ -0,0 +1,5 @@
+require 'spec_helper'
+require 'bureaucrat/fields/boolean_field'
+
+describe Bureaucrat::Fields::BooleanField do
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,4 @@
+require 'i18n'
+
 root = File.expand_path('..', __FILE__)
-I18n.load_path += Dir[File.join(root, 'locales', '**', '*.yml').to_s]
+I18n.load_path += Dir[File.join(root, 'test', 'locales', '**', '*.yml').to_s]
