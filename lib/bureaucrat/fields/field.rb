8bf8d92faae4c0a4a5b2da2d1bc561f42d7f7154
Blake Harvey <bharvey@sittercity.com>
2013-01-24 09:30:25 -0600

    Break out tooltip attr into label and field options

--- lib/bureaucrat/fields/field.rb
+++ lib/bureaucrat/fields/field.rb
@@ -51,7 +51,7 @@ module Bureaucrat
     end
 
     class Field
-      attr_accessor :sub_label, :css_class, :id, :required, :initial, :widget, :hidden_widget, :show_hidden_initial, :help_text, :tooltip, :validators, :form_name, :name
+      attr_accessor :sub_label, :css_class, :id, :required, :initial, :widget, :hidden_widget, :show_hidden_initial, :help_text, :tooltip_label, :tooltip_field, :validators, :form_name, :name
 
       def initialize(options={})
         @required = options.fetch(:required, true)
@@ -64,7 +64,8 @@ module Bureaucrat
         @css_class = options[:css_class]
         @sub_label = options[:sub_label]
         @id = options[:id]
-        @tooltip = options[:tooltip]
+        @tooltip_label = options[:tooltip_label]
+        @tooltip_field = options[:tooltip_field]
 
         @widget = @widget.new if @widget.is_a?(Class)
         @widget.attrs.update(widget_attrs(@widget))
