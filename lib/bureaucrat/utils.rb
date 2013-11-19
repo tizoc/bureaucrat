3bacda44cf544161c5b1762b9ca3e4ebbe2b5dae
Robert Grider <rgrider@sittercity.com>
2013-11-19 16:40:49 -0600

    SIT-243: SafeString.to_s is still safe

--- lib/bureaucrat/utils.rb
+++ lib/bureaucrat/utils.rb
@@ -8,6 +8,10 @@ module Bureaucrat
     class SafeString < String
       include SafeData
 
+      def to_s
+        SafeString.new(self)
+      end
+
       def +(rhs)
         rhs.is_a?(SafeString) ? SafeString.new(super(rhs)) : super(rhs)
       end
